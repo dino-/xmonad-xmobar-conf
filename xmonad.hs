9bf8424a8f9b6e3e5555b7ad7ce70212fdb60b6b
Dino Morelli <dino@ui3.info>
2010-05-13 03:20:04 +0000

    Fixed some indenting

--- xmonad.hs
+++ xmonad.hs
@@ -209,14 +209,14 @@ myLayout = avoidStruts $ tiled ||| Mirror tiled ||| Full ||| simpleTabbed
      -- default tiling algorithm partitions the screen into two panes
      tiled   = Tall nmaster delta ratio
 
-     -- The default number of windows in the master pane
-     nmaster = 1
+      -- The default number of windows in the master pane
+      nmaster = 1
 
-     -- Default proportion of screen occupied by master pane
-     ratio   = 1/2
+      -- Default proportion of screen occupied by master pane
+      ratio   = 1/2
 
-     -- Percent of screen to increment by when resizing panes
-     delta   = 3/100
+      -- Percent of screen to increment by when resizing panes
+      delta   = 3/100
 
 ------------------------------------------------------------------------
 -- Window rules:
